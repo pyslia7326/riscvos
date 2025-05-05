7e8749a10ea0f0b5ccbc172def20d389b19f0ab7
pyslia7326 <peiroaya7326@gmail.com>
2025-05-05 11:36:41 +0800

    Add task module for task management
    This commit introduces the `task` module, which provides the
    fundamental for task management in the kernel.
    
    Key features:
    - `TaskStruct`: Represents a task with fields for state, stack
      pointer, and registers.
    - `TaskState`: Enum to represent task states (e.g., `None`, `Ready`,
      `Running`, `Blocked`).
    - `Stack`: Struct to manage task stacks.
    - Constants for task management, such as `MAX_TASK_NUM` and
      `USER_STACK_SIZE`.
    
    This module lays the groundwork for multitasking in the kernel.

---
+++ src/lib/task/mod.rs
@@ -0,0 +1,100 @@
+use core::mem::offset_of;
+
+pub mod scheduler;
+
+const MAX_TASK_NUM: usize = 12;
+const USER_STACK_SIZE: usize = 4096;
+
+#[derive(Clone, Copy, PartialEq, Eq)]
+pub enum TaskState {
+    None,
+    Ready,
+    Running,
+    Blocked,
+}
+
+#[derive(Clone, Copy)]
+pub struct Stack {
+    pub stack: [u8; crate::task::USER_STACK_SIZE as usize],
+}
+
+#[derive(Clone, Copy)]
+pub struct TaskStruct {
+    pub id: Option<u64>,
+    pub state: TaskState,
+    pub stack_ptr: *mut u8,
+    pub sepc: u64,
+    pub ra: u64,
+    pub sp: u64,
+    pub gp: u64,
+    pub tp: u64,
+    pub t: [u64; 7],
+    pub s: [u64; 12],
+    pub a: [u64; 8],
+}
+
+pub const OFFSET_SEPC: usize = offset_of!(TaskStruct, sepc);
+
+pub const OFFSET_RA: usize = offset_of!(TaskStruct, ra);
+pub const OFFSET_SP: usize = offset_of!(TaskStruct, sp);
+pub const OFFSET_GP: usize = offset_of!(TaskStruct, gp);
+pub const OFFSET_TP: usize = offset_of!(TaskStruct, tp);
+pub const OFFSET_T: usize = offset_of!(TaskStruct, t);
+pub const OFFSET_S: usize = offset_of!(TaskStruct, s);
+pub const OFFSET_A: usize = offset_of!(TaskStruct, a);
+
+pub const OFFSET_T0: usize = OFFSET_T + 0 * 8;
+pub const OFFSET_T1: usize = OFFSET_T + 1 * 8;
+pub const OFFSET_T2: usize = OFFSET_T + 2 * 8;
+pub const OFFSET_T3: usize = OFFSET_T + 3 * 8;
+pub const OFFSET_T4: usize = OFFSET_T + 4 * 8;
+pub const OFFSET_T5: usize = OFFSET_T + 5 * 8;
+pub const OFFSET_T6: usize = OFFSET_T + 6 * 8;
+
+pub const OFFSET_S0: usize = OFFSET_S + 0 * 8;
+pub const OFFSET_S1: usize = OFFSET_S + 1 * 8;
+pub const OFFSET_S2: usize = OFFSET_S + 2 * 8;
+pub const OFFSET_S3: usize = OFFSET_S + 3 * 8;
+pub const OFFSET_S4: usize = OFFSET_S + 4 * 8;
+pub const OFFSET_S5: usize = OFFSET_S + 5 * 8;
+pub const OFFSET_S6: usize = OFFSET_S + 6 * 8;
+pub const OFFSET_S7: usize = OFFSET_S + 7 * 8;
+pub const OFFSET_S8: usize = OFFSET_S + 8 * 8;
+pub const OFFSET_S9: usize = OFFSET_S + 9 * 8;
+pub const OFFSET_S10: usize = OFFSET_S + 10 * 8;
+pub const OFFSET_S11: usize = OFFSET_S + 11 * 8;
+
+pub const OFFSET_A0: usize = OFFSET_A + 0 * 8;
+pub const OFFSET_A1: usize = OFFSET_A + 1 * 8;
+pub const OFFSET_A2: usize = OFFSET_A + 2 * 8;
+pub const OFFSET_A3: usize = OFFSET_A + 3 * 8;
+pub const OFFSET_A4: usize = OFFSET_A + 4 * 8;
+pub const OFFSET_A5: usize = OFFSET_A + 5 * 8;
+pub const OFFSET_A6: usize = OFFSET_A + 6 * 8;
+pub const OFFSET_A7: usize = OFFSET_A + 7 * 8;
+
+impl Stack {
+    pub const fn new() -> Self {
+        Stack {
+            stack: [0; crate::task::USER_STACK_SIZE],
+        }
+    }
+}
+
+impl TaskStruct {
+    pub const fn new() -> Self {
+        TaskStruct {
+            id: None,
+            state: TaskState::None,
+            stack_ptr: core::ptr::null_mut(),
+            sepc: 0,
+            ra: 0,
+            sp: 0,
+            gp: 0,
+            tp: 0,
+            t: [0; 7],
+            s: [0; 12],
+            a: [0; 8],
+        }
+    }
+}
