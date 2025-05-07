7b45cb4cfe877480c190feea7a9c53cd4a8f141d
pyslia7326 <peiroaya7326@gmail.com>
2025-05-07 17:45:19 +0800

    Refactor trap dispatch logic and delegate timer
    This commit moves the trap dispatch logic to `src/lib/trap/mod.rs`
    to handle both user and kernel traps in a unified manner.
    
    Key changes:
    - Added `trap_dispatch` function to handle various trap causes,
      including environment calls and interrupts.
    - Delegated M-mode timer interrupts to S-mode software interrupts
      using the `timer_handler` function.
    - Updated the logic to increment `sepc` by 4 for environment calls,
      ensuring the trap does not return to the original `ecall` instruction.
    
    This refactor centralizes trap dispatch logic and ensures proper
    handling of traps across privilege levels.

--- src/lib/trap/mod.rs
+++ src/lib/trap/mod.rs
@@ -1,3 +1,67 @@
 pub mod exception;
 pub mod interrupt;
-pub mod trap_handler;
+pub mod kernel_trap;
+pub mod user_trap;
+
+use crate::csr;
+use crate::task::scheduler::scheduler;
+use crate::task::{TaskState, TaskStruct};
+use crate::timer::timer_handler;
+use crate::uart::{print_integerln, print_string};
+
+#[unsafe(no_mangle)]
+pub fn trap_dispatch(cur_task_struct: &mut TaskStruct) {
+    let cause = cur_task_struct.xcause;
+    match cause {
+        interrupt::MACHINE_TIMER_INTERRUPT => {
+            print_string("==========================================\n");
+            print_string("  Triggered a machine timer interrupt!\n");
+            print_string("==========================================\n");
+            // timer_handler will reassign a supervisor software interrupt
+            timer_handler();
+        }
+        interrupt::SUPERVISOR_SOFTWARE_INTERRUPT => {
+            print_string("==========================================\n");
+            print_string("Triggered a supervisor software interrupt!\n");
+            print_string("==========================================\n");
+            cur_task_struct.state = TaskState::Ready;
+            let next_task_struct = scheduler();
+            csr::write_sepc(next_task_struct.xepc);
+            csr::write_sscratch(next_task_struct as *const TaskStruct as u64);
+            csr::write_sip(csr::read_sip() & !(1 << csr::SIP_SSIP));
+            print_string("switch to task pid: ");
+            print_integerln(next_task_struct.id.unwrap() as u64);
+        }
+        exception::ENVIRONMENT_CALL_FROM_U_MODE => {
+            print_string("==========================================\n");
+            print_string("    Triggered a user environment call!\n");
+            print_string("==========================================\n");
+            cur_task_struct.state = TaskState::Ready;
+            // Increment sepc to the instruction after ecall.
+            cur_task_struct.xepc += 4;
+            let next_task_struct = scheduler();
+            csr::write_sepc(next_task_struct.xepc);
+            csr::write_sscratch(next_task_struct as *const TaskStruct as u64);
+            print_string("switch to task pid: ");
+            print_integerln(next_task_struct.id.unwrap() as u64);
+        }
+        exception::ENVIRONMENT_CALL_FROM_S_MODE => {
+            print_string("==========================================\n");
+            print_string(" Triggered a supervisor environment call!\n");
+            print_string("==========================================\n");
+            // Increment sepc to the instruction after ecall.
+            cur_task_struct.xepc += 4;
+            let next_task_struct = scheduler();
+            csr::write_sepc(next_task_struct.xepc);
+            csr::write_sscratch(next_task_struct as *const TaskStruct as u64);
+            print_string("switch to task pid: ");
+            print_integerln(next_task_struct.id.unwrap() as u64);
+        }
+        _ => {
+            // Resume the current task
+            cur_task_struct.state = TaskState::Running;
+            csr::write_sepc(cur_task_struct.xepc);
+            csr::write_sscratch(cur_task_struct as *const TaskStruct as u64);
+        }
+    }
+}
