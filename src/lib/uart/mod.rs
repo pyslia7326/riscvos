743723552cac5cbdf2844fb6036c2d7ded556ebe
pyslia7326 <peiroaya7326@gmail.com>
2025-05-27 16:01:17 +0000

    Add ring buffer and SpinLock for UART output
    Introduce a 256-byte ring buffer for UART output and protect it
    with a SpinLock. Add uart_write and uart_write_buffer_flush
    functions to buffer and flush UART data safely. This improves
    UART output reliability and prevents data races.

--- src/lib/uart/mod.rs
+++ src/lib/uart/mod.rs
@@ -1,3 +1,5 @@
+use crate::mutex::Mutex;
+use crate::mutex::SpinLock;
 use core::ptr;
 
 const UART: usize = 0x10000000;
@@ -5,6 +7,42 @@ const UART_THR: *mut u8 = (UART + 0x00) as *mut u8;
 const UART_LSR: *mut u8 = (UART + 0x05) as *mut u8;
 const LSR_EMPTY_BIT: u8 = 5;
 const UART_LSR_EMPTY: u8 = 1 << LSR_EMPTY_BIT;
+
+const UART_WRITE_BUFFER_SIZE: usize = 256;
+static mut UART_WRITE_BUFFER: [u8; UART_WRITE_BUFFER_SIZE] = [0; UART_WRITE_BUFFER_SIZE];
+static mut UART_WRITE_HEAD: usize = 0;
+static mut UART_WRITE_TAIL: usize = 0;
+static UART_WRITE_LOCK: SpinLock = SpinLock::new();
+
+// Write data into the UART buffer.
+// If the buffer is full, new data will be dropped.
+pub fn uart_write(ptr: *const u8, len: usize) {
+    UART_WRITE_LOCK.lock();
+    for i in 0..len {
+        unsafe {
+            let next_tail = (UART_WRITE_TAIL + 1) % UART_WRITE_BUFFER_SIZE;
+            // If buffer is full, stop writing
+            if next_tail == UART_WRITE_HEAD {
+                break;
+            }
+            UART_WRITE_BUFFER[UART_WRITE_TAIL] = *ptr.add(i);
+            UART_WRITE_TAIL = next_tail;
+        }
+    }
+    UART_WRITE_LOCK.unlock();
+}
+
+// Flush the UART buffer: send all buffered data to UART hardware.
+pub fn uart_write_buffer_flush() {
+    UART_WRITE_LOCK.lock();
+    unsafe {
+        while UART_WRITE_HEAD != UART_WRITE_TAIL {
+            print_char(UART_WRITE_BUFFER[UART_WRITE_HEAD] as char);
+            UART_WRITE_HEAD = (UART_WRITE_HEAD + 1) % UART_WRITE_BUFFER_SIZE;
+        }
+    }
+    UART_WRITE_LOCK.unlock();
+}
 
 pub fn print_char(c: char) {
     unsafe {
