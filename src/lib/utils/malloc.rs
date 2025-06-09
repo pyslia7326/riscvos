2285e77bb3c7d2f547700c1efb07b6aadac4b754
pyslia7326 <peiroaya7326@gmail.com>
2025-06-09 11:52:58 +0800

    Align malloc allocations to 8-byte boundary
    Update the `malloc` implementation to ensure all allocated memory
    blocks are aligned to an 8-byte boundary by adding padding as needed
    before each allocation. Proper alignment is crucial to avoid undefined
    behavior and to ensure compatibility with various data types, atomic
    operations, and CPU architectures.
    
    No changes to the external interface. All allocations are now guaranteed
    to be 8-byte aligned.

--- src/lib/utils/malloc.rs
+++ src/lib/utils/malloc.rs
@@ -5,16 +5,25 @@ const HEAP_SIZE: usize = 8 * 1024;
 static HEAP: [u8; HEAP_SIZE] = [0; HEAP_SIZE];
 static mut PROGRAM_BREAK: usize = 0;
 static HEAP_LOCK: YieldLock = YieldLock::new();
+const ALIGNMENT: usize = 8;
 
 pub unsafe fn malloc(nbytes: usize) -> Option<*mut u8> {
     unsafe {
         HEAP_LOCK.lock();
-        if PROGRAM_BREAK + nbytes > HEAP_SIZE {
+        let current_program_break_addr = HEAP.as_ptr() as usize + PROGRAM_BREAK;
+        let remainder = current_program_break_addr % ALIGNMENT;
+        let padding_bytes = if remainder == 0 {
+            0
+        } else {
+            ALIGNMENT - remainder
+        };
+        let aligned_program_break_offset = PROGRAM_BREAK + padding_bytes;
+        if aligned_program_break_offset + nbytes > HEAP_SIZE {
             HEAP_LOCK.unlock();
             return None;
         }
-        let ptr = HEAP.as_ptr().add(PROGRAM_BREAK);
-        PROGRAM_BREAK += nbytes;
+        let ptr = HEAP.as_ptr().add(aligned_program_break_offset);
+        PROGRAM_BREAK = aligned_program_break_offset + nbytes;
         HEAP_LOCK.unlock();
         Some(ptr as *mut u8)
     }
